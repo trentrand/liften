0343d53a4b594e53bc06ba4107930a5dcf9fa1e0
Trent Rand <Trentmrand@gmail.com>
2015-03-30 01:18:25 -0700

    Added base implementation of main view
    + LiftinViewController

---
+++ liften/LiftinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LiftinViewController.swift
+//  liften
+//
+//  Created by Trent Rand on 3/30/15.
+//  Copyright (c) 2015 applies, llc. All rights reserved.
+//
+
+import Foundation
+
+class LiftinViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+    
+}
