9665b4248e56370fc0e3cffbec74c05e06e6b68b
Trent Rand <Trentmrand@gmail.com>
2015-03-30 01:14:36 -0700

    Revert 81acefc..6632508
    This rolls back to commit 81acefca3cc21496526139934d35a065dce57089.

---
+++ liften/RegisterViewController.swift
@@ -0,0 +1,62 @@
+//
+//  RegisterViewController.swift
+//  liften
+//
+//  Created by Trent Rand on 3/1/15.
+//  Copyright (c) 2015 applies, llc. All rights reserved.
+//
+
+import Foundation
+
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var btnDone: UIButton!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        self.navigationController?.navigationBar.hidden = false
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBar.hidden = false
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == emailTextField) {
+            passwordTextField.select(textField)
+        } else {
+            registerUser(emailTextField.text, password: passwordTextField.text)
+        }
+        return true
+    }
+    
+    func registerUser(email: String, password: String) {
+        var user = PFUser()
+        user.username = email
+        user.password = password
+        user.email = email
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, error: NSError!) -> Void in
+            if error == nil {
+                // Registration Successful
+                NSLog("User registration successful with credentials \(email) / \(password)")
+                // TODO switch to map view
+//                let viewController: ViewController = ViewController()
+//                self.showViewController(viewController, sender: self)
+            } else {
+                // Show the errorString somewhere and let the user try again.
+                var alert = UIAlertController(title: "There was a problem", message: "\(error.userInfo)", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+        btnDone.enabled = true
+    }
+
+    @IBAction func doneButton(sender: AnyObject) {
+        btnDone.enabled = false
+        registerUser(emailTextField.text, password: passwordTextField.text)
+    }
+}
